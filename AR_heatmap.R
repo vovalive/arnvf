3f08269bfe14ad5b8cf7dda7020b7e91cf6c4a98
vovalive <looongdog@gmail.com>
2015-08-26 11:31:10 +0300

    New 1-0 heatmaps

--- AR_heatmap.R
+++ AR_heatmap.R
@@ -12,7 +12,8 @@ AR.genes.desc <- read.delim2("AR.genes.desc.txt", stringsAsFactors=FALSE,quote="
 
 
 AR=AR.counts.all
-row.names(AR)=AR$name
+AR=AR[-nrow(AR),]
+row.names(AR)=AR.genes.desc$description
 AR$name=NULL
 AR=data.matrix(AR)
 colnames(AR)
@@ -26,16 +27,21 @@ for(i in 1:ncol(AR)){
 colnames(AR)
 colnames(AR)=gsub(x = colnames(AR),pattern = ".fastq.AR.sorted.bam",replacement = "")
 
-AR.100=AR[which(row.names(AR) %in% AR.covered.100pr$V2),]
+AR.100=AR[which(AR.counts.all$name %in% AR.covered.100pr$V2),]
 plot(hclust(dist(t(AR.100))))
 heatmap.2(log2(AR.100+1),trace='none',col=rev(brewer.pal(9,'Spectral')),margins=c(5,10))
 
-a=heatmap.2(log2(AR.100+1),trace='none',col=rev(brewer.pal(9,'Spectral')),margins=c(5,10))
-str(a)
-genelist=as.data.frame(rev(colnames(a$carpet)))
-names(genelist)='name'
-description=merge(genelist,AR.genes.desc,by='name',all.x = T,sort=F)
-
 pdf(file = 'AR.heatmap.pdf',width = 30,height = 15)
-heatmap.2(log2(AR.100+1),trace='none',col=rev(brewer.pal(9,'Spectral')),margins=c(5,140),labRow = description$description,keysize = .5,cexRow = 1.1)
+heatmap.2(log2(AR.100+1),trace='none',col=rev(brewer.pal(9,'Spectral')),margins=c(8,140),keysize = .5,cexRow = 1,cexCol = 1)
 dev.off()
+
+### now 0 and 1
+AR.100.01=AR.100
+AR.100.01[which(AR.100.01 > 0)]=1
+
+
+pdf(file = 'AR.heatmap.01.pdf',width = 30,height = 15)
+heatmap.2(AR.100.01,trace='none',col=rev(brewer.pal(9,'Spectral')),margins=c(8,140),keysize = .5,cexRow = 1,cexCol = 1)
+dev.off()
+
+write.table(AR.100.01,file = 'AR.covered100percent.txt',sep='\t',quote=F,col.names = NA)
--- VF_heatmap.R
+++ VF_heatmap.R
@@ -12,7 +12,8 @@ VF.genes.desc <- read.delim2("VF.genes.desc.txt", stringsAsFactors=FALSE,quote="
 
 
 VF=VF.counts.all
-row.names(VF)=VF$name
+VF=VF[-nrow(VF),]
+row.names(VF)=VF.genes.desc$description
 VF$name=NULL
 VF=data.matrix(VF)
 colnames(VF)
@@ -26,16 +27,21 @@ for(i in 1:ncol(VF)){
 colnames(VF)
 colnames(VF)=gsub(x = colnames(VF),pattern = ".fastq.VF.sorted.bam",replacement = "")
 
-VF.100=VF[which(row.names(VF) %in% VF.covered.100pr$V2),]
+VF.100=VF[which(VF.counts.all$name %in% VF.covered.100pr$V2),]
 plot(hclust(dist(t(VF.100))))
 heatmap.2(log2(VF.100+1),trace='none',col=rev(brewer.pal(9,'Spectral')),margins=c(5,10))
 
-a=heatmap.2(log2(VF.100+1),trace='none',col=rev(brewer.pal(9,'Spectral')),margins=c(5,10))
-str(a)
-genelist=as.data.frame(rev(colnames(a$carpet)))
-names(genelist)='name'
-description=merge(genelist,VF.genes.desc,by='name',all.x = T,sort=F)
-
 pdf(file = 'VF.heatmap.pdf',width = 30,height = 15)
-heatmap.2(log2(VF.100+1),trace='none',col=rev(brewer.pal(9,'Spectral')),margins=c(8,90),labRow = description$description,keysize = .5,cexRow = 1.3,cexCol = 3)
+heatmap.2(log2(VF.100+1),trace='none',col=rev(brewer.pal(9,'Spectral')),margins=c(8,90),keysize = .5,cexRow = 1.3,cexCol = 3)
 dev.off()
+
+### now 0 and 1
+VF.100.01=VF.100
+VF.100.01[which(VF.100.01 > 0)]=1
+
+
+pdf(file = 'VF.heatmap.01.pdf',width = 30,height = 15)
+heatmap.2(VF.100.01,trace='none',col=rev(brewer.pal(9,'Spectral')),margins=c(8,90),keysize = .5,cexRow = 1.3,cexCol = 3)
+dev.off()
+
+write.table(VF.100.01,file = 'VF.covered100percent.txt',sep='\t',quote=F,col.names = NA)
